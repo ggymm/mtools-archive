27beaeefab17590928c9aa03be50744cde9c5607
gongym <1967988842@qq.com>
2021-09-22 12:33:23 +0800

    添加表情包更新脚本

---
+++ scripts/update-bqb.mjs
@@ -0,0 +1,16 @@
+#!/usr/bin/env zx
+import { cd } from 'zx'
+import { join } from 'path'
+import { copyFileSync, constants } from "fs";
+
+const project_path = 'C:/Product/mtools'
+const bqb_path = 'D:/Code/github/ChineseBQB'
+
+// 首先更新表情包
+cd(bqb_path)
+await $`git pull`
+
+// 拷贝索引文件到固定位置
+await copyFileSync(join(bqb_path, 'chinesebqb_v2fy.json'),
+  join(project_path, 'src/renderer/assets/chinesebqb_v2fy.json'), constants.COPYFILE_FICLONE)
+
