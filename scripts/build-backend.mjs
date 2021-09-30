8197a3d973ac29a64dcb7eb91aa481d5c92dd27a
gongym <1967988842@qq.com>
2021-09-30 11:13:58 +0800

    添加打包后端服务脚本

---
+++ scripts/build-backend.mjs
@@ -0,0 +1,28 @@
+#!/usr/bin/env zx
+import { $, cd } from 'zx'
+import { join } from 'path'
+import { copyFileSync, constants } from 'fs'
+
+const mtools = 'C:\\Product\\mtools'
+const backend = 'C:\\Product\\mtools-backend'
+
+// 打包
+cd(backend)
+await $`go build -ldflags "-s -w" -o _build\\release\\`
+
+// 压缩
+cd(join(backend, '_build'))
+await $`upx -9 release\\mtools-backend.exe`
+
+// 拷贝数据库
+copyFileSync(
+  join(backend, 'mtools-backend.db'),
+  join(mtools, 'public', 'backend', 'mtools-backend.db'),
+  constants.COPYFILE_FICLONE
+)
+// 拷贝服务端文件
+copyFileSync(
+  join(backend, '_build', 'release', 'mtools-backend.exe'),
+  join(mtools, 'public', 'backend', 'mtools-backend.exe'),
+  constants.COPYFILE_FICLONE
+)
