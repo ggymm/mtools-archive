6a2ef4aa35c1fc832bdc6dded2f8d6a7815460db
gongym <1967988842@qq.com>
2021-09-30 11:13:35 +0800

    更新表情包脚本更新

--- scripts/update-bqb.mjs
+++ scripts/update-bqb.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env zx
-import { cd } from 'zx'
+import { $, cd } from 'zx'
 import { join } from 'path'
-import { copyFileSync, constants } from "fs";
+import { copyFileSync, constants } from 'fs'
 
 const project_path = 'C:/Product/mtools'
 const bqb_path = 'D:/Code/github/ChineseBQB'
@@ -11,6 +11,9 @@ cd(bqb_path)
 await $`git pull`
 
 // 拷贝索引文件到固定位置
-await copyFileSync(join(bqb_path, 'chinesebqb_v2fy.json'),
-  join(project_path, 'src/renderer/assets/chinesebqb_v2fy.json'), constants.COPYFILE_FICLONE)
+await copyFileSync(
+  join(bqb_path, 'chinesebqb_v2fy.json'),
+  join(project_path, 'src/renderer/assets/chinesebqb_v2fy.json'),
+  constants.COPYFILE_FICLONE
+)
 
